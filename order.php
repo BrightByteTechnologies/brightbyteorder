5b57a243b3412a1c82df26a476c25e501b57de1a
LetsGaming <bergmanncrafter@gmail.com>
2023-04-29 03:03:49 +0200

    Better error handling

--- order.php
+++ order.php
@@ -1,5 +1,9 @@
 <?php
-
+$errorMessages = [
+    '<h1>An error occurred!</h1> <p1>It seems like the QR-Code provided is invalid!<p1>',
+    '<h1>An error occurred!</h1> <p1>The QR-Code has already been used!<p1>',
+    '<h1>An error occurred!</h1> <p1>QR-Code is not registered for this table!<p1>'
+];
 if (isset($_GET['token']) && isset($_GET['tableNo'])) {
     $token = $_GET['token'];
     $tableNo = $_GET['tableNo'];
@@ -11,12 +15,14 @@ if (isset($_GET['token']) && isset($_GET['tableNo'])) {
 
     $codeValid = false;
     if (count($data) < 1) {
-        echo ("<h1>An error occurred!</h2><p1>It sems like the QR-Code provided is invalid!</p1>");
+        echo $errorMessages[0];
     } elseif ($data[0]['used'] != 0) {
-        echo ("<h1>An error occurred!</h2><p1>The QR-Code has already been used!</p1>");
+        echo $errorMessages[1];
+    } elseif ($data[0]['tableNo'] != $tableNo) {
+        echo $errorMessages[2];
     } else {
         $codeValid = true;
-    }
+    }    
 }
 
 ?>
